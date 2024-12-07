32f4b9c0fddb4a9d02c717afddcd92561076a78a
Mountains <Mountains97@foxmail.com>
2024-12-07 16:39:03 +0800

    fix: Reply already submitted

--- packages/in_app_purchase/in_app_purchase_android/android/src/main/java/io/flutter/plugins/inapppurchase/MethodCallHandlerImpl.java
+++ packages/in_app_purchase/in_app_purchase_android/android/src/main/java/io/flutter/plugins/inapppurchase/MethodCallHandlerImpl.java
@@ -46,6 +46,7 @@ import io.flutter.plugins.inapppurchase.Messages.PlatformPurchasesResponse;
 import io.flutter.plugins.inapppurchase.Messages.PlatformQueryProduct;
 import io.flutter.plugins.inapppurchase.Messages.Result;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.HashMap;
 import java.util.List;
 
@@ -229,11 +230,13 @@ class MethodCallHandlerImpl implements Application.ActivityLifecycleCallbacks, I
     }
 
     try {
+      final AtomicBoolean callbackInvoked = new AtomicBoolean(false);
       QueryProductDetailsParams params =
           QueryProductDetailsParams.newBuilder().setProductList(toProductList(products)).build();
       billingClient.queryProductDetailsAsync(
           params,
           (billingResult, productDetailsList) -> {
+            if (!callbackInvoked.compareAndSet(false, true)) return;
             updateCachedProducts(productDetailsList);
             final PlatformProductDetailsResponse.Builder responseBuilder =
                 new PlatformProductDetailsResponse.Builder()
@@ -411,11 +414,13 @@ class MethodCallHandlerImpl implements Application.ActivityLifecycleCallbacks, I
       // Like in our connect call, consider the billing client responding a "success" here
       // regardless
       // of status code.
+      final AtomicBoolean callbackInvoked = new AtomicBoolean(false);
       QueryPurchasesParams.Builder paramsBuilder = QueryPurchasesParams.newBuilder();
       paramsBuilder.setProductType(toProductTypeString(productType));
       billingClient.queryPurchasesAsync(
           paramsBuilder.build(),
           (billingResult, purchasesList) -> {
+            if (!callbackInvoked.compareAndSet(false, true)) return;
             PlatformPurchasesResponse.Builder builder =
                 new PlatformPurchasesResponse.Builder()
                     .setBillingResult(fromBillingResult(billingResult))
@@ -437,11 +442,13 @@ class MethodCallHandlerImpl implements Application.ActivityLifecycleCallbacks, I
     }
 
     try {
+      final AtomicBoolean callbackInvoked = new AtomicBoolean(false);
       billingClient.queryPurchaseHistoryAsync(
           QueryPurchaseHistoryParams.newBuilder()
               .setProductType(toProductTypeString(productType))
               .build(),
           (billingResult, purchasesList) -> {
+            if (!callbackInvoked.compareAndSet(false, true)) return;
             PlatformPurchaseHistoryResponse.Builder builder =
                 new PlatformPurchaseHistoryResponse.Builder()
                     .setBillingResult(fromBillingResult(billingResult))
